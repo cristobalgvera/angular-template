c4174d0ec1b8568066c0c85646d2bf2bca669843
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-12-18 00:11:47 -0300

    feat(angular): improve base app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions({ skipInitialTransition: true }),
+    ),
     provideAnimations(),
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
   ],
 };
